2bd6b6433afd99054412973158e20a0dfcbfa206
NathaliaMachado <nathaliamachd@gmail.com>
2019-07-18 14:17:51 -0400

    logout

---
+++ src/components/Logout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {browserHistory} from  'react-router';
+
+export default class Logout extends Component{
+
+    componentDidMount() {
+        localStorage.removeItem('auth-token');
+        browserHistory.push('/');
+    }
+
+    render() {
+        return null;
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './css/timeline.css';
 import './css/login.css';
 import './css/reset.css';
 import Login from './components/Login.js';
+import Logout from './components/Logout.js';
 import {Router, Route, browserHistory} from 'react-router';
+
 
 function verificaAutenticacao(nextState,replace) {
     if(localStorage.getItem('auth-token') === null){
-      replace('/?msg=você precisa estar logado para acessar o endereço');
+      replace('/?msg=You must be logged');
     }
   }
 
@@ -18,6 +20,7 @@ ReactDOM.render(
         <Router history={browserHistory}>
             <Route path="/" component={Login}/>
             <Route path="/timeline" component={App} onEnter={verificaAutenticacao} />
+            <Route path="/logout" component={Logout}/>
         </Router>
     ), 
     document.getElementById('root')
