9b64a6e96f8184e9d82afe2bc47162af346755bd
NathaliaMachado <nathaliamachd@gmail.com>
2019-08-06 14:50:54 -0400

    redux implemented

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
 import { CSSTransitionGroup } from 'react-transition-group';
+import TimelineApi from '../logicas/TimelineApi';
 
 export default class Timeline extends Component {
 
@@ -12,7 +13,7 @@ export default class Timeline extends Component {
 
     componentWillMount() {
         this.props.store.subscribe(fotos => {
-            this.setState({fotos});
+            this.setState({fotos:this.props.store.getState()});
         });  
     }
 
@@ -24,7 +25,8 @@ export default class Timeline extends Component {
         } else {
             urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
         }
-        this.props.store.lista(urlPerfil);
+        this.props.store.dispatch(TimelineApi.lista(urlPerfil));                  
+
     }
 
 
@@ -41,11 +43,11 @@ export default class Timeline extends Component {
     }
 
     like(fotoId) {
-        this.props.store.like(fotoId); 
+        this.props.store.dispatch(TimelineApi.like(fotoId)); 
     }
 
     comenta(fotoId,textoComentario) {
-       this.props.store.comenta(fotoId, textoComentario);
+      this.props.store.dispatch(TimelineApi.comenta(fotoId,textoComentario));
     }
 
     render() {
